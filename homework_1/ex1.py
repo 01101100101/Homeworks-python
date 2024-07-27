8abd88cbfb25901eb4332066ed5af23253d3f301
Ivan Krasnikov <ivan.krasnikov2019@gmail.com>
2024-07-27 11:25:47 +0300

    Upgrade of hw1 ex1 after lecture 2

--- homework_1/ex1.py
+++ homework_1/ex1.py
@@ -8,8 +8,5 @@ if n <= 0:
     exit()
 
 for i in range(1, n+1):
-    for j in range(n-i):
-        print(' ', end='')
-    for k in range(i*2-1):
-        print('*', end='')
-    print()
+    print(' ' * (n-i), '*' * (i*2-1), sep='')
+
